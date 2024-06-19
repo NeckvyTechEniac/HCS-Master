3188faca78da8ed07ee6a0f33ac426123a280e5f
NeckvyTechEniac <neckvy@techeniac.com>
2024-06-19 16:47:40 +0530

    feat: stackbit config file

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,4 @@
-import { defineStackbitConfig } from "@stackbit/types";
+import { SiteMapEntry, defineStackbitConfig, getLocalizedFieldForLocale } from "@stackbit/types";
 
 export default defineStackbitConfig({
     stackbitVersion:'~0.6.0',
@@ -7,7 +7,24 @@ export default defineStackbitConfig({
     {    name:'page',
         type:'page',
         urlPath:'/{slug}'}
-    ]
+    ],
+    siteMap: ({ documents, models }) => {
+    const pageModels = models.filter(m => m.type === "page").map(m => m.name);
+    return documents
+      .filter(d => pageModels.includes(d.modelName))
+      .map(document => {
+        const slug = getLocalizedFieldForLocale(document.fields.slug);
+        if (!slug) return null;
+        const urlPath = "/" + slug.value.replace(/^\/+/, "");
+        return {
+          stableId: document.id,
+          urlPath,
+          document,
+          isHomePage: urlPath === "/"
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
 },
 )
 
