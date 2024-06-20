51fa6f0d5222aede48f139c8d68e61819672ba92
NeckvyTechEniac <neckvy@techeniac.com>
2024-06-20 11:26:24 +0530

    fix: config file

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,36 +1,36 @@
-import { SiteMapEntry, defineStackbitConfig, getLocalizedFieldForLocale } from "@stackbit/types";
+import {  defineStackbitConfig } from "@stackbit/types";
 
 export default defineStackbitConfig({
     stackbitVersion: '~0.6.0',
     ssgName: 'nextjs',
-    models: {
-        page: {
-            type: 'page',
-            label: 'Page',
-            fields: [
-                { type: 'string', name: 'title', label: 'Title', required: true },
-                { type: 'string', name: 'slug', label: 'Slug', required: true },
-                // Add other fields as per your model requirements
-            ]
-        },
-        person: {
-            type: 'data',
-            label: 'Person',
-            fields: [
-                { type: 'string', name: 'name', label: 'Name', required: true },
-                // Add other fields as per your model requirements
-            ]
-        },
-        // Add other models if needed
-    },
-    modelExtensions: [
-        {
-            name: 'page',
-            type: 'page',
-            urlPath: '/{slug}'
-        },
-        // Add other model extensions if needed
-    ],
+    // models: {
+    //     page: {
+    //         type: 'page',
+    //         label: 'Page',
+    //         fields: [
+    //             { type: 'string', name: 'title', label: 'Title', required: true },
+    //             { type: 'string', name: 'slug', label: 'Slug', required: true },
+    //             // Add other fields as per your model requirements
+    //         ]
+    //     },
+    //     person: {
+    //         type: 'data',
+    //         label: 'Person',
+    //         fields: [
+    //             { type: 'string', name: 'name', label: 'Name', required: true },
+    //             // Add other fields as per your model requirements
+    //         ]
+    //     },
+    //     // Add other models if needed
+    // },
+    // modelExtensions: [
+    //     {
+    //         name: 'page',
+    //         type: 'page',
+    //         urlPath: '/{slug}'
+    //     },
+    //     // Add other model extensions if needed
+    // ],
     // contentSources: [
     //     {
     //         name: 'pages',
